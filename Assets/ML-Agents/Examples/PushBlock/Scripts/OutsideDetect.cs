8eccbb041ec56cdbc27c464d7ce764089e2c5082
Pranav Mujumdar <psmujumdar@gmail.com>
2020-03-22 23:15:00 -0400

    removed the penguins and Added the trained nn and summaries for the push block

---
+++ Assets/ML-Agents/Examples/PushBlock/Scripts/OutsideDetect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class OutsideDetect : MonoBehaviour
+{
+    /// <summary>
+    /// The associated agent.
+    /// This will be set by the agent script on Initialization.
+    /// Don't need to manually set.
+    /// </summary>
+    [HideInInspector]
+    public PushAgentBasic agent;
+
+    void OnCollisionEnter(Collision col)
+    {
+        // Touched goal.
+        if (col.gameObject.CompareTag("outside"))
+        {
+            agent.WentOutside();
+        }
+    }
+}
